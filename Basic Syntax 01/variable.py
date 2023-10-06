8de00e90508230ab323f2eb673e802e8e623b1f6
pronobsarker <sarkpr01@gettysburg.edu>
2023-10-06 11:44:40 -0400

    first commit

---
+++ Basic Syntax 01/variable.py
@@ -0,0 +1,28 @@
+#basic changes than java
+# NO NEED to write its type. 
+# Syntax --> var_name = value
+# type(variable_name) this shows the type of the variable
+
+""" multi line comment in python: syntax--> option + shift + A  (""" """)
+    Single line comment --> # or Command + /
+"""
+# variables
+age = 12
+interest_rate = 5.5
+name = "kashem"
+district = 'Bogra'
+is_single = True
+is_sleeping = False
+
+# Printing
+
+print(age)
+print(name, district, is_single, interest_rate)
+
+text = f"Hello I'm {name}. I live in {district}. my interest rate is {interest_rate}"
+
+print(text)
+
+
+print(type(district)) #shows the type of the variable
+
